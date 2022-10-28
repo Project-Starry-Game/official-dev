1b3ab14418c0b76a43b184a08a54b24efae92a79
a88dsz598 <casualattitude0@buho.game.tw>
2022-10-28 14:26:34 +0800

    Test

--- src/components/trailer.vue
+++ src/components/trailer.vue
@@ -1,57 +1,65 @@
 <template>
-    <div class="trailer_container">
-        <div class="section section-1" ref="first">
-            <h1>STARRY</h1>
-        </div>
-
-        <div class="section section-2" ref="second">
-            <div class="section2_container">
-                <p>Swallow the Black liquid, tell me what you see. </p>
-                <p>A beautiful world with silence? A broken coffin ?Or even just that empty bottle in your hand.</p>
-            </div>
-        </div>    
-
-        <div class="img_container">
-            <v-img class="background" ref="background" src="@/assets/building.png" />
-            <v-img class="foreground" ref="foreground" src="@/assets/character.png" />
-        </div>
+  <v-container
+    class="ma-0 pa-0"
+    fluid
+    style="overflow: scroll; max-height: 95vh"
+  >
+    <div class="section section-1" ref="first">
+      <h1>STARRY</h1>
     </div>
 
+    <div class="section section-2" ref="second" style="margin-bottom: -70px">
+      <div class="section2_container">
+        <p>Swallow the Black liquid, tell me what you see.</p>
+        <p>
+          A beautiful world with silence? A broken coffin ?Or even just that
+          empty bottle in your hand.
+        </p>
+      </div>
+    </div>
+
+    <div class="img_container">
+      <v-img class="foreground" ref="foreground" src="@/assets/character.png" />
+      <v-img class="background" ref="background" src="@/assets/building.png" />
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "Home",
-    components: {},   
-}
+  name: "Home",
+  components: {},
+};
 </script>
 
 <style>
-.trailer_container{
-    background: #2E231B;
-    overflow: scroll;
-    /* overflow-x: hidden; */
-    max-height: 90vh;
+.trailer_container {
+  background: #2e231b;
+  overflow: scroll;
+  /* overflow-x: hidden; */
+  max-height: 90vh;
 }
 .trailer_container::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
-img.background,
-img.foreground{
-    top:0;
-    position: sticky;
+img.background {
+  position: sticky;
 }
-img.foreground{
-    width: 100%;
+.foreground {
+  position: sticky;
+  top: 100%;
+  left: 45%;
+  width: 10%;
+  z-index: 100;
 }
-.section{
-    color: #F2CA95;
-    display: flex;
-    justify-content: center;
+.section {
+  color: #f2ca95;
+  display: flex;
+  justify-content: center;
 }
-.section2_container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.section2_container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
