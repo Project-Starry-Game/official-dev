1e401e65aef08e58634c75d53fc1d775439b3fb9
Tangchii <1124chi11224chi@gmail.com>
2022-11-23 12:22:12 +0800

    sss

--- src/components/Home/trailer.vue
+++ src/components/Home/trailer.vue
@@ -6,12 +6,6 @@
       id="scroll-target"
       class="ad_container overflow-y-auto pa-0 ma-0"
     >
-<<<<<<< HEAD
-      <v-row justify="center" align="center">
-        <v-subheader class="sub pa-5">Offset Top {{ offsetTop }} </v-subheader>
-      </v-row>
-
-=======
     <v-row
       justify="center"
       align="center"
@@ -19,7 +13,6 @@
         <v-subheader class="sub pa-5">Offset Top  {{ offsetTop }} </v-subheader>
     </v-row>
     <!-- building_img -->
->>>>>>> fb169b9 (get the photo)
       <div class="background">
         <v-img
           class="background"
@@ -29,35 +22,12 @@
         />
       </div>
 
-<<<<<<< HEAD
-      <!-- logo -->
-      <div class="img_container" id="logo_box">
-        <div class="img_logo" id="img_logo">
-=======
     <!-- logo -->
        <div class="img_container" id="logo_box">
         <div class="img_logo" id="img_logo" >
->>>>>>> fb169b9 (get the photo)
           <v-img class="logo" id="logo" ref="first" :src="butterflylogo" />
         </div>
       </div>
-
-<<<<<<< HEAD
-      <!-- test -->
-      <div style="background-color: black">
-        <div
-          class="text_section"
-          id="text_section"
-          data-aos="fade-in"
-          data-aos-duration="1000"
-        >
-          <p>Swallow the Black liquid, tell me what you see.</p>
-          <p>
-            A beautiful world with silence? A broken coffin ?Or even just that
-            empty bottle in your hand.
-          </p>
-        </div>
-=======
     <!-- test -->
       <div
         class="text_section"
@@ -70,7 +40,7 @@
           A beautiful world with silence? A broken coffin ?Or even just that
           empty bottle in your hand.
         </p>
->>>>>>> fb169b9 (get the photo)
+
       </div>
     </v-container>
   </div>
@@ -150,15 +120,9 @@ export default {
   /* background-color: rgb(0, 0, 0); */
   color: rgb(255, 255, 255);
   width: 100vw;
-<<<<<<< HEAD
-  height: 100vh;
-  position: absolute;
-  top: 0%;
-=======
   height: 80vh;
   position: sticky;
   bottom: 50%;
->>>>>>> fb169b9 (get the photo)
   z-index: 1000;
   display: flex;
   flex-direction: column;
