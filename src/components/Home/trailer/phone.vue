474667493486954e5d7f08a3a075a59739c89d40
Tangchii <1124chi11224chi@gmail.com>
2022-12-02 15:08:27 +0800

    phone bg rwd

--- src/components/Home/trailer/phone.vue
+++ src/components/Home/trailer/phone.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-container class="trailer pa-0 ma-0" fluid>
+  <div class="trailer" >
     <div class="butter_logo_container pa-0 ma-0">
       <v-img :src="butterflylogo" class="butter_logo" />
     </div>
+   
     <div class="butter_bg">
-      <v-img :src="bg_builder" />
+
+      <v-img class="butter_bg" cover :src="bg_builder" />
     </div>
     <div class="scrolldown_box">
       <v-btn @click="scrollToNextSection()" color="transparent" elevation="0">
@@ -13,13 +15,13 @@
       <div class="scrolldown_text">Scroll Down</div>
     </div>
     <div class="text_section_phone">
-      <p>Swallow the Black liquid, tell me what you see.</p>
-      <p>
-        A beautiful world with silence? A broken coffin ?Or even just that empty
-        bottle in your hand.
-      </p>
-    </div>
-  </v-container>
+      <p>Swallow the Black liquid, </p>
+      <p>tell me what you see.</p>
+      <br>
+      <p>A beautiful world with silence? A broken coffin ? </p>
+      <p>Or even just that empty bottle in your hand.</p>
+    </div> 
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -45,9 +47,17 @@ export default {
 .trailer {
   position: relative;
   top: 0;
+  border: 1px solid rgb(255, 140, 0);
+  display: flex;
+  flex-direction: column;
   overflow-x: hidden;
   overflow-y: hidden;
-  max-height: 100vh;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+
+  background-color: rgb(230, 207, 207);
 }
 .butter_logo_container {
   position: absolute;
@@ -65,12 +75,27 @@ export default {
   top: 10vh;
 }
 .butter_bg {
-  top: 0;
-  margin: auto;
+  height: 100%;
+  max-width: 100%;
+  object-position: center bottom;
 }
 .text_section_phone {
+  border: 1px solid seagreen;
+  height: 80%;
+  width: 80%;
+  margin: 1%;
+  position: fixed;
+  top: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  align-self: center;
+  font-size: 0.7rem;
+  color: rgb(255, 255, 255);
 }
 .scrolldown_box {
+  border: 1px solid seagreen;
   position: absolute;
   bottom: 10vh;
   width: 100vw;
@@ -98,6 +123,7 @@ a#scroll-btn {
     opacity: 0;
   }
 }
+
 .scrolldown_text {
   font-size: 1rem;
   color: #fff;
