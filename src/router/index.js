493437794dedd8fc9fdb3a5621697a04e2d55d1d
a88dsz598 <casualattitude0@buho.game.tw>
2022-12-04 17:48:16 +0800

    Setup 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from "../views/Home.vue";
 import HomePage from "../views/HomePage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
